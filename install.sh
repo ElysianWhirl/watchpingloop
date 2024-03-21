cd48add853013b33a90dbe851fecaec56fa69844
ElysianWhirl <158003859+ElysianWhirl@users.noreply.github.com>
2024-03-21 20:40:53 +0700

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/ash
+# Installation script by Aulian
+
+DIR=/usr/bin
+
+
+finish()
+
+download_files()
+{
+    	clear
+    	echo "Downloading files from repo watchpingloop..."
+     	wget -O $DIR/watchpingloop https://raw.githubusercontent.com/ElysianWhirl/watchpingloop/main/watchpingloop.sh && chmod +x $DIR/watchpingloop
+      finish
+}
+
+echo ""
+echo "Install Script code from repo Aulian."
+
+while true; do
+    read -p "This will download the files into $DIR. Do you want to continue (y/n)? " yn
+    case $yn in
+        [Yy]* ) download_files; break;;
+        [Nn]* ) exit;;
+        * ) echo "Please answer 'y' or 'n'.";;
+    esac
+done
